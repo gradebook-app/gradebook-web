7caae9fec808ec3a7acc5ea8236a9fca81ebb85f
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-212.local>
2021-12-05 14:26:12 -0500

    Updated Static Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 
@@ -10,11 +10,6 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/privacy" component={PrivacyPolicy} />
-        <Redirect from="/sitemap.xml" to="/static/sitemap.xml" />
-        <Route path="/static/sitemap.xml"/>
-           <Redirect from="/robots.xml" to="/static/sitemap.xml" />
-        <Route path="/static/robots.xml" />
-        <Redirect to="/" />
       </Switch>
     </Router>
   );
