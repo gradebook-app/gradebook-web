0b084da771fa7eb9c537b42dbcce47125afaafa8
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-212.local>
2021-12-05 14:19:19 -0500

    Updated Routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-d
 import Home from './pages/Home';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 
+const reload = () => window.location.reload();
+
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/privacy" component={PrivacyPolicy} />
+        <Redirect from="/sitemap.xml" to="/static/sitemap.xml" />
+        <Route path="/static/sitemap.xml" render={() => {
+          reload();
+          return null;
+        }} />
         <Redirect to="/" />
       </Switch>
     </Router>
